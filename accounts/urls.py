51d69d9395aad1dc4f6607327afbce177cd5b49f
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2026-04-20 15:44:30 +0200

    feat(accounts): add isolated Account endpoints and routing

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from accounts.views import AccountViewSet
+
+
+router = DefaultRouter()
+router.register(r'accounts', AccountViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from accounts.serializers import AccountSerializer
+from accounts.models import Account
 
 # Create your views here.
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+    
+
